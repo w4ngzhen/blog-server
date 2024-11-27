9791ca84686a9eeb43b77818ca532c3329621025
w4ngzhen <w4ngzhen@hotmail.com>
2024-11-27 08:48:08 +0800

    feat: 调整路由

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
-import callback from './routes/callback.ts';
-import proxy from './routes/proxy.ts';
+import callback from './routes/gh/callback.ts';
+import proxy from './routes/gh/proxy.ts';
 
 const app = new Hono();
 
@@ -10,7 +10,7 @@ app.use('*', cors());
 
 app.get('/', (c) => c.text('Hello Blog Comment Server!'));
 
-app.all('/proxy/:link{.*}', proxy);
-app.get('/callback', callback);
+app.all('/gh/proxy/:link{.*}', proxy);
+app.get('/gh/callback', callback);
 
 export default app;
